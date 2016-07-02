fbdf54c9796c1ee91a1169483bd0a1cb18d9bb89
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-07-02 13:07:01 +0800

    Clean up index.js.

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-'use strict'
-global.Promise = require('bluebird')
-global.ROOT = __dirname
+'use strict';
 
-const http = require('http')
-const express = require('express')
+global.Promise = require('bluebird');
+global.ROOT = __dirname;
 
-const bodyParser = require('body-parser')
+const http = require('http');
+const bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
-const logger = require('morgan')
+const env = process.env.NODE_ENV || 'development';
+const port = process.env.PORT || 3000;
 
-const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
+const app = express();
 
-const env = process.env.NODE_ENV || 'development'
-const port = process.env.PORT || 3000
-
-const app = express()
-app.locals.ENV = env
-app.locals.ENV_DEVELOPMENT = (env === 'development')
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = (env === 'development');
 
 app.use(logger('dev'));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(`${ROOT}/public`))
+app.use(express.static(`${ROOT}/public`));
 if (env === 'development') {
-  const compiler = webpack(require('./webpack.config.dev'));
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    stats: { colors: true }
-  }));
-  app.use(webpackHotMiddleware(compiler));
+    const compiler = webpack(require('./webpack.config.dev'));
+    app.use(webpackDevMiddleware(compiler, {
+        noInfo: true,
+        stats: {
+            colors: true
+        }
+    }));
+    app.use(webpackHotMiddleware(compiler));
 }
 
-app.use((err, req, res, next) => {
-  res.status(500).send(err.stack);
-  console.error(err);
+app.use((err, req, res) => {
+    res.status(500).send(err.stack);
+    console.error(err);
 });
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log(`==> Listen on port ${server.address().port}`);
-});
+server.listen(port, () => console.log(`==> Listening on port ${server.address().port}`));
